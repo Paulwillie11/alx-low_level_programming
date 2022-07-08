1538ec01d5fe266c7012c42456c829bf7c7d871c
Paulwillie11 <106773569+Paulwillie11@users.noreply.github.com>
2022-07-08 09:22:42 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - prints numbers from 0 -9
- */
+*print_numbers - print 0123456789
+*
+*Return: void
+*/
+
 void print_numbers(void)
 {
 	char c;
-	
-	for (c = 48; c < 58; c++)
-		_putchar(c);
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }
