7ffcc41199b32e1f34c67fcf87d889321882cf74
Paulwillie11 <106773569+Paulwillie11@users.noreply.github.com>
2022-07-22 18:16:54 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,53 +1,26 @@
-#include <stdio.h>
-#include "holberton.h"
-/**
- * _strlen - returns the length of a string
- * @str: string
- * Return: return the length
- */
-int _strlen(char *str)
-{
-int length = 0;
+#include "main.h"
 
-while (*str)
-{
-str++;
-length++;
-}
-return (length);
-}
 /**
- * _strncat - conctenates two strings
- * @dest: destination pointer
- * @src: pointer to a string
- * @n: amount to be concatenated
- * Return: concatenated string
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-char *cat  = dest + _strlen(dest);
-int length;
+	int i, j;
 
-if (n > _strlen(src) + _strlen(dest))
-{
-length = _strlen(dest) + _strlen(src);
-}
-else
-{
-length = _strlen(dest) + n;
-}
-while (*src && n > 0)
-{
-*cat += *src;
-src++;
-cat++;
-n--;
-}
-if (n > 0)
-{
-*cat += '\0';
-}
-cat -= (length);
-*dest = *cat;
-return (cat);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
