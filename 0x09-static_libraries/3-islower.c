f13c9927466b78cd1ba9e907256e59d6bcda334f
Paulwillie11 <106773569+Paulwillie11@users.noreply.github.com>
2022-07-22 18:23:29 +0100

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,10 +1,14 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _islower - returns 1 if c is lowercase, 0 otherwise
- * @c: integer to be tested
- * Return: 1 or 0
+ * _islower -check if a character is lowercase.
+ * @c: The character to be checked.
+ * Return: 1 if character is lowercase, o otherwise.
  */
 int _islower(int c)
 {
-return (c >= 97 && c <= 122);
+	if (c >= 97 && c <= 122)
+	return (1);
+	else
+	return (0);
 }
