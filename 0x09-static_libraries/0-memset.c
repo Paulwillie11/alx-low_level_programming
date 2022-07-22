76870a3fdb788f98cb3bd97c17407708c20b7381
Paulwillie11 <106773569+Paulwillie11@users.noreply.github.com>
2022-07-22 18:11:55 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,17 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ * @n: bytes of the memory area pointed to by @s
+ * @s: with the constant byte @b
+ * @b: memory area pointer
+ * Return: a pointer to the memory area @s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i = 0;
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
